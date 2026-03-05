74df5fe71aeac5492b5b3ce7648deaa2c8f87a94
Fereshta Khawari <fereshtakhawari41@gmail.com>
2026-03-05 10:17:38 +0430

    Sloution for Assignment 01 of Day 02 of week 08

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -1,4 +1,17 @@
 function bmiCalc(w,h){
-    bmi=w/(h*h);
-    return bmi;
+    var bmi = Math.round(w / (h * h));
+   
+    if (bmi < 18.5) {
+        return "Your BMI is " + bmi + ", so you are underweight";
+    } 
+    else if (bmi <= 24.9) {
+        return "Your BMI is " + bmi + ", so you have a normal weight";
+    } 
+    else {
+        return "Your BMI is " + bmi + ", so you are overweight";
+    }
 }
+let w = Number(prompt("Enter your weight (kg):"));
+let h = Number(prompt("Enter your height (m):"));
+var result = bmiCalc(w,h);
+alert(result);
